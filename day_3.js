dcc3392ed47182fa55f2aaa658b96a53aca299bc
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-02 23:55:03 +0530

    day 3 code

---
+++ day_3.js
@@ -0,0 +1,18 @@
+//Difference between  let , var and const 
+
+const name = "one"
+let email = "one@gmail.com"
+var password = "123456"
+
+// name = "two"
+email = "two@gmail.com"
+password = "abcdef"
+
+console.log(name, email, password);
+
+
+
+
+// Why not to use the var ?
+// It has issue on block scope and functional scope  
+
