e0252ce625a989ce4638aa30a2ae89f96d22e159
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-03 20:37:12 +0530

    Day 4 primitive data types

---
+++ day_4.js
@@ -0,0 +1,18 @@
+//Data types in javascript
+
+//Primitive data types
+
+// Short cut to remember -> NN SS BB U 
+
+let name = "Ankit"
+let age = 21
+let isStudent = true
+let x;
+let y = null
+let num = BigInt(234)
+let id = Symbol("id")
+
+console.table([name, age, isStudent, x, y, num, id]);
+
+console.log(typeof x);
+console.log(typeof id);
