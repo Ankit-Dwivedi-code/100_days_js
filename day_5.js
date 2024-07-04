c6133c715b53ae14167c77af524bc4a574d6ba91
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-04 10:18:36 +0530

    Day 5 non primitive data type

---
+++ day_5.js
@@ -0,0 +1,20 @@
+//Non primitive data type ---> Object
+
+// In JavaScript, all non-primitive data types are actually objects, including arrays and functions.
+
+//Example
+
+
+//Objects store collections of key-value pairs.
+
+let person = {
+    name : "Ankit",
+    age : 21
+}
+
+console.log(person);
+console.log(person["age"]);
+console.log(person.name);
+console.log(Object.keys(person));
+
+
