8b18ac9ed5c01ab19badcb5e626bac3d784e03c5
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-05 23:50:16 +0530

    day 6 code data type conversion

---
+++ day_6.js
@@ -0,0 +1,21 @@
+//Data type conversion in js
+
+
+//String to number
+let str = "345"
+console.log(typeof str);
+
+let num2 = Number(str)
+console.log(num2);
+console.log(typeof num2);
+
+
+
+//Number to boolean
+let x = 1
+
+let bool = Boolean(x)
+console.log(bool);
+console.log(typeof bool);
+
+
